80ee9d11444f781be0187df5f76b31b7db24d87e
Smiles99 <mdavies10@stuy.edu>
2020-12-17 23:16:25 -0500

    Create Sorts.java

---
+++ Sorts.java
@@ -0,0 +1,49 @@
+import java.util.Random;
+import java.util.Arrays;
+
+public class Sorts {
+    /**Bubble sort of an int array.
+     *@postcondition The array will be modified such that the elements will be in increasing order.
+     *@param data  the elements to be sorted.
+     */
+    public static void bubbleSort(int[] data) {
+        int x = 0;
+        int y = 1;
+        int z = data.length;
+        int hold = 0;
+        while (z > 1) {
+            while (y < z) {
+                if (data[x] > data[y]) {
+                    hold = data[y];
+                    data[y] = data[x];
+                    data[x] = hold;
+                    x++;
+                    y++;
+                } else {
+                    x++;
+                    y++;
+                }
+            }
+            x = 0;
+            y = 1;
+            z--;
+        }
+        String answer = Arrays.toString(data);
+        System.out.print(answer);
+    }
+}
+
+class Tester {
+    public static void main(String[] args) {
+        Random len = new Random();
+        int x = len.nextInt(100);
+        int[] test1 = new int[x];
+        Random place = new Random();
+        for(int i = 0; i < test1.length; i++) {
+            test1[i] = place.nextInt(100);
+        }
+        String answer1 = Arrays.toString(test1);
+        System.out.println(answer1);
+        Sorts.bubbleSort(test1);
+    }
+}
