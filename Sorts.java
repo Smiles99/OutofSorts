73c1bd11ca06751996df93acde1380001f7dbc5a
Miles Davies <73114747+Smiles99@users.noreply.github.com>
2021-01-01 16:23:07 -0500

    Update Sorts.java

--- Sorts.java
+++ Sorts.java
@@ -1,5 +1,4 @@
-import java.util.Random;
-import java.util.Arrays;
+import java.util.*;
 
 public class Sorts {
     /**Bubble sort of an int array.
@@ -28,8 +27,8 @@ public class Sorts {
             y = 1;
             z--;
         }
-        String answer = Arrays.toString(data);
-        System.out.println(answer);
+        //String answer = Arrays.toString(data);
+        //System.out.println(answer);
     }
 
     public static void selectionSort(int[] data) {
@@ -56,65 +55,48 @@ public class Sorts {
             i = x;
             min = Integer.MAX_VALUE;
         }
-        String answer = Arrays.toString(data);
-        System.out.println(answer);
+        //String answer = Arrays.toString(data);
+        //System.out.println(answer);
     }
 
     public static void insertionSort(int[] data) {
-        int x = 1;
-        int hold = 0;
-        int sorted = 0;
-        int y = sorted;
-        while (x < data.length) {
-            if (data[x] < data[y]) {
-                while (y > 0) {
-                    if (data[y] < data[x]) {
-                        hold = data[y];
-                        data[y] = data[x];
-                        data[x] = hold;
-                        y = 0;
-                    } else {
-                        y--;
-                    }
-                }
-                x++;
-                sorted++;
-                y = sorted;
-            } else {
-                hold = data[y];
-                data[y] = data[x];
-                data[x] = hold;
-                x++;
-                sorted++;
-                y = sorted;
+        for (int i = 1; i < data.length; ++i) {
+            int sorted = data[i];
+            int j = i - 1;
+            while (j >= 0 && data[j] > sorted) {
+                data[j + 1] = data[j];
+                j = j - 1;
             }
+            data[j + 1] = sorted;
         }
-        x = data.length - 1;
-        y = 0;
-        while (y < data.length) {
-            hold = data[y];
-            data[y] = data[x];
-            data[x] = hold;
-            y++;
-        }
-        String answer = Arrays.toString(data);
-        System.out.println(answer);
+//        String answer = Arrays.toString(data);
+//        System.out.println(answer);
     }
 }
 
-class Tester {
-    public static void main(String[] args) {
-        Random len = new Random();
-        int x = len.nextInt(10);
-        int[] test1 = new int[x];
-        Random place = new Random();
-        for(int i = 0; i < test1.length; i++) {
-            test1[i] = place.nextInt(10);
-        }
-        String answer1 = Arrays.toString(test1);
-        System.out.println(answer1);
-        Sorts.bubbleSort(test1);
-        Sorts.selectionSort(test1);
-        Sorts.insertionSort(test1);
-    }
-}
+//class Tester {
+//    public static void main(String[] args) {
+//        Random len = new Random();
+//        int x = len.nextInt(10);
+//        int[] test1 = new int[x];
+//        Random place = new Random();
+//        for(int i = 0; i < test1.length; i++) {
+//            test1[i] = place.nextInt(100000);
+//        }
+//        int arr[] = new int[10];
+//        arr[0] = (int)(Math.random() * 10000);
+//        for (int i = 1; i < arr.length; i++) {
+//            arr[i] = (int)(Math.random() * arr[i - 1]);
+//        }
+//        String answer1 = Arrays.toString(test1);
+//        System.out.println(answer1);
+//        Sorts.bubbleSort(test1);
+//        Sorts.selectionSort(test1);
+//        Sorts.insertionSort(test1);
+//        String answer2 = Arrays.toString(arr);
+//        System.out.println(answer2);
+//        Sorts.bubbleSort(arr);
+//        Sorts.selectionSort(arr);
+//        Sorts.insertionSort(arr);
+//    }
+//}
